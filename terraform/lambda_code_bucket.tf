10b4e1297c264d230d10bb6f28a01cd7d81f4d8f
Ian Cowan <ian@cowan.aero>
2025-04-23 19:05:36 -0400

    Build module gha workflow

--- terraform/lambda_code_bucket.tf
+++ terraform/lambda_code_bucket.tf
@@ -16,3 +16,31 @@ resource "aws_s3_bucket_lifecycle_configuration" "aviationapi-source-code-lifecy
     }
   }
 }
+
+resource "aws_iam_group" "aviationapi-source-code-access-group" {
+  name = "BuildS3BucketAccess"
+}
+
+resource "aws_iam_policy" "aviationapi-source-code-policy" {
+  name = "aviationapi-source-code-role-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "S3GetPutObject"
+        Effect   = "Allow"
+        Action   = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = "${aws_s3_bucket.aviationapi-source-code.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_group_policy_attachment" "aviationapi-source-code-role-group" {
+  group = aws_iam_group.aviationapi-source-code-access-group.name
+  policy_arn = aws_iam_policy.aviationapi-source-code-policy.arn
+}
