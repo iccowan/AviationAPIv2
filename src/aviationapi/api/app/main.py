ca04995a1197a06dd8b8792c3837cc5c5a4e4455
Ian Cowan <ian@cowan.aero>
2025-06-21 17:00:09 -0600

    #17 Allow CORs from anywhere

--- src/aviationapi/api/app/main.py
+++ src/aviationapi/api/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
 from .v2 import main as v2
 
 app = FastAPI()
 handler = Mangum(app)
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+)
 
 app.mount("/v2", v2.app)
